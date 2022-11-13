056ffe7497202ae60733c696eddc1618b4208456
artmizu <hello@artmizu.ru>
2022-11-13 04:00:48 +0400

    minor

---
+++ test/fixture/nuxt-2/app.vue
@@ -0,0 +1,13 @@
+<script>
+import { useNuxtApp } from '#app'
+
+const ctx = useNuxtApp()
+if (process.client)
+  ctx.$metrika.reachGoal('zzz')
+</script>
+
+<template>
+  <div>
+    <bla-bla-bla />
+  </div>
+</template>
